97b821c109b0f0cb0d480ee52fc3e6badf5f4c85
argus <tjdals4047@gmail.com>
2023-10-03 18:54:23 +0900

    v2

--- HW1/skeleton.py
+++ HW1/skeleton.py
@@ -2,7 +2,6 @@ import os
 import math
 import sys
 import time
-import heapq
 
 
 RAM_SIZE = 10000 # number of lines : 100Byte * 10000 = 1MB
@@ -10,39 +9,25 @@ RAM_SIZE = 10000 # number of lines : 100Byte * 10000 = 1MB
 
 def split_file(input_file_name):
 
-    # run file 개수, io 횟수, 읽은 line 초기화
-    run, io, l = 0, 0, 0
-
-    # 정렬을 위해 min heap 사용
-    min_heap = []
+    # run file 개수, io 횟수
+    run, io = 0, 0
 
     # input file을 열어서 읽음
     with open(input_file_name, 'r') as input_file:
 
-        # 아래 과정 반복
         while True:
+            lines = input_file.readlines(RAM_SIZE*101-1)    # RAM_SIZE만큼 읽음
+            if not lines: break
 
-            line = input_file.readline()                # 한 줄 읽음
-            if not line: break                          # 만약 다 읽었다면, 종료
+            lines.sort()                                    # 정렬
+            io += len(lines)                                # disk io 횟수 +
 
-            key = line[:10]                             # 정렬 기준인 key 할당 
-            heapq.heappush(min_heap, (key, line))       # min heap에 데이터 추가
-            l += 1                                      # 읽은 line +1
-            io += 1                                     # disk io 횟수 +1
-
-            # 만약 하나의 run file 크기만큼 읽었으면 run file에 데이터 write
-            if l%RAM_SIZE == 0:                         
-                
-                # disk 폴더에 "pass0_R1.txt"와 같이 기록
-                with open(f"./disk/pass0_R{run+1}.txt", 'w') as diskFile:
-
-                    # min_heap에 있는 line을 diskFile에 write
-                    while min_heap:
-                        key, line = heapq.heappop(min_heap)
-                        diskFile.write(line)
-                        io += 1                         # disk io 횟수 +1
-                
-                run += 1                                # run file 개수 +1
+            # disk 폴더에 "pass0_R1.txt"와 같이 기록
+            with open(f"./disk/pass0_R{run+1}.txt", 'w') as diskFile:
+                diskFile.writelines(lines)
+                io += len(lines)                            # disk io 횟수 +
+            
+            run += 1                                        # run file 개수 +1
 
     # 통계량 출력            
     print_pass_statistics(0, run, io)
@@ -71,62 +56,82 @@ def external_merge_sort():
         run_num = math.ceil(len(total_disk_files) / BUFFER_NUM)
 
         # 3. 각 pass마다 initializing해야하는 것들
-        #   io : disk read/write count
-        #   start : 하나의 run을 만들기 위해 몇번째 disk file부터 읽어야 하는지 체크
         io, start = 0, 0
 
         # PASS 시작
         for run in range(run_num):
             
-            # 하나의 run을 만드는 과정
             # 이번 run에서 읽어야할 diskfile
             disk_files = total_disk_files[start:start+BUFFER_NUM]
+            disk_files_cursor = [0 for _ in range(len(disk_files))]
             
-            original_files, read_count, flag_count= len(disk_files), 0, 0
+            # disk file과 cursor 정보를 모두 담고 있는 dictionary
+            disk_cursor_info = dict(zip(disk_files, disk_files_cursor))
+            
+            buffers = [[] for _ in range(BUFFER_NUM)]
+            disk_buffer_info = dict(zip(disk_files, buffers))
 
-            # 만들어야 하는 run file 수 만큼 iterate
-            while True:
+            # 이번에 읽어야하는 disk_files
+            active_disk_files = disk_files
+            completed_disk_files = []
 
-                min_heap = []
+            with open(f"./disk/pass{pass_cnt+1}_R{run+1}.txt", 'w') as newDiskFile:
+                # 만들어야 하는 run file 수 만큼 iterate
+                while True:
 
-                # 읽어야하는 diskfile 마다 buffer size만큼의 line을 읽어서 sort
-                for disk_file in disk_files:
+                    # 읽어야하는 diskfile 마다 buffer size만큼의 line을 읽어서 buffer에 저장
+                    for disk_file in active_disk_files:
+                        if disk_file not in completed_disk_files:
+                            cursor = disk_cursor_info[disk_file]
+
+                            # run에서 buffer_size만큼 line을 읽어서 buffers에 저장
+                            with open(f"./disk/{disk_file}", 'r') as file:
+                            
+                                # 이전까지 읽었던 위치 찾기 : 몇번째 읽는 건지 * 한줄의 길이(101)* 한번에 읽는 line 수(buffer_size*10)
+                                file.seek(cursor*101*BUFFER_SIZE*10, 0)
+                                
+                                # BUFFER_SIZE에 해당하는 line만큼 읽기
+                                lines = file.readlines(BUFFER_SIZE*10*101-1)
+
+                                # 만약 다 run file을 읽었다면 완료 목록에 추가
+                                if not lines:
+                                    completed_disk_files.append(disk_file)
+                                    # disk_buffer_info[disk_file] = ["DONE"]
+                                    continue
+
+                                disk_buffer_info[disk_file] += lines
+                                io += len(lines)
+
+                                disk_cursor_info[disk_file] += 1
                     
-                    # run에서 buffer_size만큼 line을 읽어서 buffers에 저장
-                    with open(f"./disk/{disk_file}", 'r') as file:
-                        # 이전까지 읽었던 위치 찾기 : 몇번째 읽는 건지 * 한줄의 길이(101)* 한번에 읽는 line 수(buffer_size*10)
-                        file.seek(read_count*101*BUFFER_SIZE*10, 0)
-                        
-                        # BUFFER_SIZE에 해당하는 line만큼 읽기
-                        for _ in range(BUFFER_SIZE*10):
-                            line = file.readline()
+                    # 모든 run file을 다 읽었으면 종료
+                    if len(completed_disk_files) == len(disk_files):
+                        break
 
-                            # 만약 run file의 크기가 작은 파일이 있으면 나머지 파일만 계속 읽어야 함
-                            if not line: 
-                                flag_count += 1
-                                disk_files.remove(disk_file)
-                                break
+                    #데이터 sorting   
+                    while True:
+                        min_value = "A"
+                        for file, buffer in disk_buffer_info.items():
+                            
+                            if file not in completed_disk_files:
+                                min_value = min(min_value, buffer[0])
+                                if min_value == buffer[0]:
+                                    min_buffer = file
+                            # else:
+                            #     min_value = min(min_value, buffer[0])
+                            #     if min_value == buffer[0]:
+                            #         a = 1
 
-                            key = line[:10]
-                            heapq.heappush(min_heap, (key, line))
-                            io += 1
-                    
-                    # 모든 파일을 다 읽으면 종료
-                    if flag_count == original_files: break
-
-                # 모든 파일을 다 읽으면 종료
-                if flag_count == original_files: break    
-
-                #sort된 데이터를 run 파일에 기록 (이어서)   
-                with open(f"./disk/pass{pass_cnt+1}_R{run+1}.txt", 'a') as diskFile:
-                    while min_heap:
-                        key, line = heapq.heappop(min_heap)
-                        diskFile.write(line)
+                        newDiskFile.write(min_value)
                         io += 1
-                
-                read_count += 1
 
-            #run 종료
+                        disk_buffer_info[min_buffer].remove(min_value)
+
+                        # 만약 이번에 가장 작은걸 제거한 buffer가 0이 되면, active_disk_files에 넣어서 buffer에 추가함
+                        if len(disk_buffer_info[min_buffer]) == 0:
+                            active_disk_files = [min_buffer]
+                            break
+
             start += BUFFER_NUM
             
         # PASS 종료
@@ -151,7 +156,7 @@ if __name__ == '__main__':
     
     # argument parsing
     n = int(sys.argv[1]) # size of buffer
-    
+
     # remove files in disk folder
     for file in os.listdir("./disk"):
         os.remove(f"./disk/{file}")
@@ -160,7 +165,7 @@ if __name__ == '__main__':
     start_time = time.time()
     
     # 1. split phase
-    split_file("input_file.txt")
+    split_file("./input_file.txt")
     
     # 2. Merge phase
     external_merge_sort()
