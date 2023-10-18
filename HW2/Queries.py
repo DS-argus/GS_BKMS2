59547edd5ed27a68a7e1ce6024f3276479518655
argus <tjdals4047@gmail.com>
2023-10-18 18:01:32 +0900

    HW2

--- HW2/Queries.py
+++ HW2/Queries.py
@@ -1,13 +1,29 @@
 import pandas as pd
 
-
 def query1():
     emp = pd.read_csv("emp.csv")
     emp = emp.sort_values(by=['DEPTNO', 'EMPNO'])
-    emp['ACC_SAL'] = emp.groupby('DEPTNO')['SAL'].rolling(window=3, min_periods=1, center=True).sum().reset_index(level=0, drop=True)
+    emp = emp[['DEPTNO', 'EMPNO', 'ENAME', 'SAL']]
 
-    print(emp[['DEPTNO', 'EMPNO', 'ENAME', 'SAL', 'ACC_SAL']].to_string(index=False))
+    acc_sal_col = []
+    for dept in emp['DEPTNO'].unique():
+        each_dept = emp[emp['DEPTNO']==dept]
+        size = len(each_dept.index)
+        i = 0
 
+        while i<size:
+            acc_sal = 0
+            acc_sal += each_dept.iloc[i]['SAL']
+            if i-1>=0: acc_sal += each_dept.iloc[i-1]['SAL']
+            if i+1<size: acc_sal += each_dept.iloc[i+1]['SAL']
+
+            acc_sal_col.append(acc_sal)
+            i += 1
+        
+    emp['ACC_SAL'] = acc_sal_col
+
+    print(emp.to_string(index=False))
+            
     return
 
 def query2():
@@ -17,17 +33,23 @@ def query2():
     result = []
     for comp in stock['company'].unique():
         each_comp = stock[stock['company']==comp]
-        
-        for idx in range(len(each_comp)-2):
-            current_price = each_comp.iloc[idx]['price']
-            next_price = each_comp.iloc[idx+1]['price']
-            nnext_price = each_comp.iloc[idx+2]['price']
+        size = len(each_comp.index)
+        i = 0
+
+        while i<size-2:
+            current_price = each_comp.iloc[i]['price']
+            next_price = each_comp.iloc[i+1]['price']
+            nnext_price = each_comp.iloc[i+2]['price']
             
             if current_price <= 50 and next_price <= current_price*0.9 and nnext_price >= next_price*1.05:
-                result += each_comp.iloc[idx:idx+3].values.tolist()
-    
+                result += each_comp.iloc[i:i+3].values.tolist()
+                i += 3          # Default : "AFTER MATCH SKIP PAST LAST ROW"
+            else:
+                i += 1
+            
     print(pd.DataFrame(result, columns=['company', 'price_date', 'price']).to_string(index=False))
 
+    return
 
 query1()
 query2()
